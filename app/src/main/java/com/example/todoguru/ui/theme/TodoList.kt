32f101d85f85a55d06ba6974b649f7c1b55844bc
teotia-codes <priyanshu.teotia50@gmail.com>
2024-06-01 08:30:37 +0530

    done with UI part of first screen

--- app/src/main/java/com/example/todoguru/ui/theme/TodoList.kt
+++ app/src/main/java/com/example/todoguru/ui/theme/TodoList.kt
@@ -2,21 +2,21 @@ package com.example.todoguru.ui.theme
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,24 +31,63 @@ import java.util.Locale
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TodoListPage() {
-    var todoList = getFakeTodo()
-    Column {
-        LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
-            itemsIndexed(todoList){index, item->
-                Card(onClick = { /*TODO*/ }) {
-                    Column {
-                        Text(text =item.title, fontWeight = FontWeight.ExtraBold,
-                            fontSize = 24.sp, color = Color.White)
-                        Text(text = item.description, color = Color.White)
-                        Text(text =SimpleDateFormat("HH:mm:aa,dd/mm", Locale.ENGLISH).format(item.createdAt.toString()) )
+    val todoList = getFakeTodo()
+    Scaffold(
+        contentColor = Color.Black,
+        containerColor = Color.Black,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "TodoGuru",
+                        color = Color.White,
+                        fontSize = 44.sp,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(Color.Black)
+            )
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
+                    itemsIndexed(todoList) { index, item ->
+                        Card(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.padding(8.dp),
+                            colors = CardDefaults.cardColors(containerColor = item.color)
+                        ) {
+                            Column(modifier = Modifier.padding(16.dp)) {
+                                Text(
+                                    text = item.title,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    fontSize = 24.sp,
+                                    color = Color.White
+                                )
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(
+                                    text = item.description,
+                                    color = Color.White
+                                )
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Row {
+                                    Text(text = "CreatedAt      ", color = Color.White, fontWeight = FontWeight.Bold)
+                                    Text(
+                                        text = SimpleDateFormat("HH:mm aa, dd/MM", Locale.ENGLISH)
+                                            .format(item.createdAt),
+                                        color = Color.White, 
+                                        fontWeight = FontWeight.ExtraBold
+                                    )
+                                }
 
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
                     }
-
-                }
-                Spacer(modifier = Modifier.height(10.dp))
+                })
             }
-        })
-    }
+        }
+    )
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
